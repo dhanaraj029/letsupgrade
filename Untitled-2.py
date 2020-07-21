3b2b8121d96fe04cb4c4ec8e73f97f29b98d0853
dhanaraj029 <68578088+dhanaraj029@users.noreply.github.com>
2020-07-21 10:08:18 -0700

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,16 @@
+name="dhanaraj"
+marks=92.2929
+age=25
+print("the name of person is",name,"marks is ",marks,"age is ",age)
+
+print("the name of person is %s marks is %f age is %d"%(name ,marks,age))
+
+print("the name of person is %10s marks is %10.5f age is %d"%(name ,marks,age))
+
+
+x=285
+y=285
+x==y
+
+str1 ="python programming"
+"k" in str1
